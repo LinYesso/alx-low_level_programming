f025c593218fe03b840c552c2c3c1b4bc5438b3e
LinYesso <lindaangeaminatayesso@gmail.com>
2022-03-16 16:27:57 -0700

    adding 5-main.c 5-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of the input 
+ * print_last_digit - prints the last digit of the input
  * @n: number to be computed
  *
  * Return: the last digit of n
@@ -11,7 +11,9 @@ int print_last_digit(int n)
 	int last = n % 10;
 	
 	if (n < 0)
+	{
 		last = -last;
+	}
 	_putchar(last + '0');
 	return (last);
 }
