1429ad4d95c392823cb88273fba18f450b53ab7c
LinYesso <lindaangeaminatayesso@gmail.com>
2022-03-18 09:42:10 -0700

    adding 2-main.c 2-mul files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: int as parameter
+ * @b: int as parameter
+ *
+ * Return: the result of a*b
+ *
+ */
+int mul(int a, int b)
+{
+	int c;
+	
+	c = a * b;
+	
+	return (c);
+}
