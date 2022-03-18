c9123c939dff468b5a6fbf2a13fd3404137dd19f
LinYesso <lindaangeaminatayesso@gmail.com>
2022-03-17 22:39:21 -0700

    the first task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - assigns a int n 
+ * @n  - argumant
+ *
+ */
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+}
